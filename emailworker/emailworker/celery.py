cd724b5fcdc6ff3bfa5d3fc6ab4265f64d6e49b7
Ritviksahay <bhagwantmaan453@gmail.com>
2025-02-14 11:43:15 +0530

    debugging

--- emailworker/emailworker/celery.py
+++ emailworker/emailworker/celery.py
@@ -5,6 +5,7 @@ from kombu import Connection
 import ssl
 import logging
 from celery.signals import task_failure
+from celery.signals import task_prerun, task_success, task_failure
 
 logger = logging.getLogger(__name__)
 
@@ -23,8 +24,17 @@ app.conf.broker_transport_options = {"visibility_timeout": 3600}  # Optional
 app.conf.broker_use_ssl = broker_use_ssl
 
 
+@task_prerun.connect
+def log_task_start(sender=None, task_id=None, task=None, **kwargs):
+    logger.info(f"Celery task {task_id} started: {task.name}")
+
+@task_success.connect
+def log_task_success(sender=None, task_id=None, result=None, **kwargs):
+    logger.info(f"Celery task {task_id} completed successfully with result: {result}")
+
 @task_failure.connect
-def handle_task_failure(sender=None, task_id=None, exception=None, **kwargs):
+def log_task_failure(sender=None, task_id=None, exception=None, **kwargs):
     logger.error(f"Celery task {task_id} failed: {exception}")
+
 
 
